27a136e5a0e458b8ce5d506eb751014753f9b86a
LotsaGeese <129383168+LotsaGeese@users.noreply.github.com>
2026-02-19 15:16:29 +1030

    Update kiwixstart.sh

--- kiwixstart.sh
+++ kiwixstart.sh
@@ -1,33 +1,37 @@
 #!/bin/sh
 
-# Download ZIM if DOWNLOAD is set
-if [ ! -z "$DOWNLOAD" ]; then
-    if [ ! -w /data ]; then
-        echo "'/data' directory is not writable by '$(id -n -u):$(id -n -g)' ($(id -u):$(id -g))."
+# Download if necessary a file
+if [ ! -z "$DOWNLOAD" ]
+then
+    # Check if /data is writable
+    if [ ! -w /data ]
+    then
+        echo "'/data' directory is not writable by '$(id -n -u):$(id -n -g)' ($(id -u):$(id -g)). ZIM file(s) can not be written."
         exit 1
     fi
-    ZIM=$(basename "$DOWNLOAD")
-    wget "$DOWNLOAD" -O "/data/$ZIM"
 
-    if [ "$#" -eq 0 ]; then
-        set -- "$@" "/data/$ZIM"
+    # Dwonload ZIM file
+    ZIM=`basename $DOWNLOAD`
+    wget $DOWNLOAD -O "$ZIM"
+
+    # Set arguments
+    if [ "$#" -eq "0" ]
+    then
+        set -- "$@" $ZIM
     fi
 fi
 
-
-PORT=${PORT:-8080}
-
-# If no arguments are passed, default to all ZIMs in /data
-if [ "$#" -eq 0 ]; then
-    set -- /data/*.zim
+if [ -z "$PORT" ]
+then
+    PORT=8080
 fi
+CMD="/usr/local/bin/kiwix-serve --port=$PORT $@"
+echo $CMD
+$CMD
 
-# Execute Kiwix
-exec /usr/local/bin/kiwix-serve --port="$PORT" "$@"
-
-
-# If Kiwix fails, show /data contents
-if [ $? -ne 0 ]; then
+# If error, print the content of /data
+if [ $? -ne 0 ]
+then
     echo "Here is the content of /data:"
     find /data -type f
 fi
